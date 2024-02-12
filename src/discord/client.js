43c7096f64b75a6be73f65eb7f10ea2d1bc08415
François-Xavier Guillois <internet@fxguillois.email>
2024-02-12 10:33:25 +0100

    add command auto-prompt to bot

--- src/discord/client.js
+++ src/discord/client.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
+const { Client, GatewayIntentBits, ChannelType, SlashCommandBuilder } = require('discord.js');
 module.exports = {
     database: null,
     name: 'sync',
@@ -20,12 +20,17 @@ module.exports = {
             databaseURL: process.env.FIREBASE_DATABASE_URL
         });
         this.database = firebaseAdmin.database();
+        
+        this.execCommands();
 
         return new Promise((resolve) => {
             this.client.once('ready', async () => {
+                this.defineCommands();
+
                 console.log('Logged in as ' + this.client.user.tag);
                 resolve();
             });
+            
             // Log the bot in (will trigger its presence online in the Discord server)
             this.client.login(process.env.DISCORD_TOKEN);
         });
@@ -167,6 +172,48 @@ module.exports = {
         }));
     },
 
+    defineCommands: async function () {
+        const autoPromptData = new SlashCommandBuilder()
+        .setName('auto-prompt')
+        .setDescription('Sends prompt message to API')
+        .addStringOption(option =>
+            option.setName('trigger')
+                .setDescription('the espanso trigger')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('url')
+                .setDescription('URL of the discord message to send')
+                .setRequired(true));
+            
+        // Adding the command globally
+        await this.client.application.commands.create(autoPromptData);
+    },  
+    
+    execCommands: async function (interaction) {
+        this.client.on('interactionCreate', async interaction => {
+            if (!interaction.isChatInputCommand()) return;
+            // when the interaction is from the 'auto-prompt' command, extract the url from the options, get the message content and send it to API
+            if (interaction.commandName === 'auto-prompt') {
+                const urlparts = interaction.options.getString('url').split('/');
+                const messageId = urlparts[urlparts.length - 1];
+                const channelId = urlparts[urlparts.length - 2];
+                const guildId = urlparts[urlparts.length - 3];
+        
+                if (messageId !== undefined && channelId !== undefined && guildId !== undefined) {
+                    const guild = await this.client.guilds.fetch(guildId);
+                    const channel = await guild.channels.fetch(channelId);
+                    const message = await channel.messages.fetch(messageId);
+                    const prompt = interaction.options.getString('trigger');
+                    const response = message.content;
+                    if(response.length > 0) {
+                        // console.log(response);
+                        // updateEspansoConfig(prompt, response);
+                    }    
+                    await interaction.reply({ content: `J'ai envoyé le prompt ${prompt} à l'API`, ephemeral: true });
+                }
+            }
+        });
+    },
     logout: function () {
         this.client.destroy();
         console.log('Logged out!');
